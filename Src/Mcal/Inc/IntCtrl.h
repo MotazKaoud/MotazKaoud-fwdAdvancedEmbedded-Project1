4ef7e215df24d55672eeea984e6dc3a3851007fe
motazkaoud <kaoudmotaz@gmail.com>
2022-08-21 01:19:59 +0200

    first commit!

---
+++ Src/Mcal/Inc/IntCtrl.h
@@ -0,0 +1,8 @@
+#ifndef INTCTRL_H
+#define INTCTRL_H
+
+#include "IntCtrl_Types.h"
+
+void IntCtrl_Init (void) ;
+
+#endif
---
+++ Src/Mcal/IntCtrl.c
@@ -0,0 +1,8 @@
+#include "IntCtrl.h"
+
+void IntCtrl_Init (void) 
+{
+
+
+	return;
+}
